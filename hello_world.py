911bcd3817daf2778518ad90f16a8bcbe3cc8c07
Matias <Matias.karjalainen@tuni.fi>
2023-09-06 14:27:18 +0300

    delete few rows in helloworld

--- hello_world.py
+++ hello_world.py
@@ -1,18 +1,6 @@
-print("Hello world!")
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("Hello darkness my old friend")
-print("I've come to talk with you again")
-=======
-=======
->>>>>>> devel
 print("Not another hello world")
 print("No")
 print("More")
 print("Hello")
 print("World")
 print("Please, I'm begging you")
-<<<<<<< HEAD
->>>>>>> devel
-=======
->>>>>>> devel
